0f8fcdb9ea71954abf1c115ca1314c05e556c4a8
UENO, M. <eunosium-1128@live.jp>
2026-01-21 03:39:44 +0900

    Refactor build script for improved clarity

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,8 @@
-mkdir build
-if errorlevel 1 exit 1
-cd build
-if errorlevel 1 exit 1
-cmake %CMAKE_ARGS% -GNinja ^
-  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-  ..
-if errorlevel 1 exit 1
+@echo on
 
-ninja -j%CPU_COUNT%
+cmake -S . -B build -G Ninja %CMAKE_ARGS%
 if errorlevel 1 exit 1
-ninja install
+cmake --build build --parallel %CPU_COUNT%
+if errorlevel 1 exit 1
+cmake --install build
 if errorlevel 1 exit 1
