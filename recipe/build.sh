3365c77ebb554d4553fc0f1cc4fc72a32ea1d2b0
Jarrett Johnson <jarrett.johnson@schrodinger.com>
2026-02-02 23:34:03 -0500

    enable builds on osx-64 and osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,12 @@
 set -exo pipefail
 
+# Rely on the Vulkan Loader during runtime rather than
+# MoltenVK during build.
+# https://github.com/KhronosGroup/Vulkan-Tools/pull/1204
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    CMAKE_ARGS="$CMAKE_ARGS -DAPPLE_USE_SYSTEM_ICD=ON"
+fi
+
 cmake -S . -B build ${CMAKE_ARGS}
 cmake --build build --parallel ${CPU_COUNT}
 cmake --install build
